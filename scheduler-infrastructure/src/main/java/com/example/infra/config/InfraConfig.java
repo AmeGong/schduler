ee9679ccd0d37765b547bc963b9afeb80b3e9070
AmeGong <1419649650@qq.com>
2023-05-07 19:47:39 +0800

    bugfix

--- scheduler-infrastructure/src/main/java/com/example/infra/config/InfraConfig.java
+++ scheduler-infrastructure/src/main/java/com/example/infra/config/InfraConfig.java
@@ -1,9 +1,14 @@
 package com.example.infra.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 import javax.sql.DataSource;
@@ -32,28 +37,28 @@ public class InfraConfig {
         dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
-//
-//    @Bean
-//    public SqlSessionFactory sqlSessionFactory() throws Exception {
-//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-//        factoryBean.setDataSource(dataSource());
-//        return factoryBean.getObject();
-//    }
+
+   @Bean
+   public SqlSessionFactory sqlSessionFactory() throws Exception {
+       SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+       factoryBean.setDataSource(dataSource());
+       return factoryBean.getObject();
+   }
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
     }
 
-//    @Bean
-//    public PlatformTransactionManager transactionManager() {
-//        return new DataSourceTransactionManager(dataSource());
-//    }
-//
-//    @Bean
-//    public TransactionTemplate transactionTemplate() {
-//        return new TransactionTemplate(transactionManager());
-//
-//    }
+   @Bean
+   public PlatformTransactionManager transactionManager() {
+       return new DataSourceTransactionManager(dataSource());
+   }
+
+   @Bean
+   public TransactionTemplate transactionTemplate() {
+       return new TransactionTemplate(transactionManager());
+
+   }
 
 }
